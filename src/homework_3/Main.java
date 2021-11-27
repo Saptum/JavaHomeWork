f7751fe66e5740c22e397b8d4fad7964e5bd9696
Your Name <you@example.com>
2021-11-28 00:17:20 +0200

    new project

---
+++ src/homework_3/Main.java
@@ -0,0 +1,12 @@
+package homework_3;
+
+public class Main {
+    public static void main(String[] args) {
+        IntList myList = new IntArrayList();
+
+        for (int i = 0; i < 100000; i++) {
+            myList.add(i);
+
+        }
+    }
+}
