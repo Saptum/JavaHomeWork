20e2a4d52b537f78d06f9cfd2749d85629c88933
Your Name <you@example.com>
2021-11-28 14:54:30 +0200

    removed all test classes

--- src/homework_6/App.java
+++
@@ -1,9 +0,0 @@
-package homework_6;
-
-public class App {
-    public static void main(String[] args) {
-
-        Demo.test(new Child());
-    }
-}
-
--- src/homework_6/Parent.java
+++
@@ -1,8 +0,0 @@
-package homework_6;
-
-import java.io.IOException;
-
-public class Parent {
-    public void f() throws Exception {}
-}
-
