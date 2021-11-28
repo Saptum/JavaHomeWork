d8c3720016589e7639182f3a8c02051cf43f38bc
Your Name <you@example.com>
2021-11-28 22:02:12 +0200

    mastering exceptions in java

---
+++ src/homework_6/TryCatch.java
@@ -0,0 +1,11 @@
+package homework_6;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        int catNumber;
+        int zero;
+        catNumber = 1;
+        zero = 0;
+        int result = catNumber/zero;
+    }
+}
