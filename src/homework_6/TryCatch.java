129cb3706af578533c893e788961257ae3fe4e60
Your Name <you@example.com>
2021-11-28 22:08:37 +0200

    Поместим проблемный код в блок try, а в блоке catch обработаем исключение.

--- src/homework_6/TryCatch.java
+++ src/homework_6/TryCatch.java
@@ -1,11 +1,19 @@
 package homework_6;
 
 public class TryCatch {
+
+
     public static void main(String[] args) {
         int catNumber;
         int zero;
-        catNumber = 1;
-        zero = 0;
-        int result = catNumber/zero;
+        try {
+            catNumber=1;
+            zero =0;
+            int result =catNumber/zero;
+            System.out.println("Не увидите это сообщение!");
+        }catch (ArithmeticException e){
+            System.out.println("Нельзя котов делить на ноль!");
+        }
+        System.out.println("Жизнь продолжается");
     }
 }
