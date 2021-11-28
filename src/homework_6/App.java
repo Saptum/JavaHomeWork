17a632d9b774bd79fe835ab5fc3ef4e27620f5d6
Your Name <you@example.com>
2021-11-28 14:37:55 +0200

    java: unreported exception java.lang.Throwable; must be caught or declared to be thrown

--- src/homework_6/App.java
+++ src/homework_6/App.java
@@ -1,15 +1,13 @@
 package homework_6;
 
-import java.io.EOFException;
-import java.io.FileNotFoundException;
-
 public class App {
-public static void fo(Throwable t) throws Exception{
-    throw t;
-}
-
-    public static void f1(Object ref) {
-        char c = ref.charAt(0);
+    public static void main(String[] args) throws Exception {
+        try {
+            Throwable t = new Exception();
+            throw t;
+        } catch (Exception e) {
+            System.out.println("Перехвачено!");
+        }
     }
 }
 
