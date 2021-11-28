c737406d9e7d6f199e904ca279c62ddd83eb241f
Your Name <you@example.com>
2021-11-28 14:48:56 +0200

    Code now work

--- src/homework_6/App.java
+++ src/homework_6/App.java
@@ -2,7 +2,7 @@ package homework_6;
 
 public class App {
     public static void main(String[] args) {
-        // тут все компилируется, Demo.test хотел Parent и мы дали ему подтип - Child
+
         Demo.test(new Child());
     }
 }
--- src/homework_6/Child.java
+++ src/homework_6/Child.java
@@ -1,5 +1,5 @@
 package homework_6;
 
 public class Child extends Parent{
-    public void f() throws Throwable {}
+ //   public void f() throws Throwable {}
 }
