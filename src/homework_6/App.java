c0a8903b300faacc91e5882f448a66856027c3a7
Your Name <you@example.com>
2021-11-28 14:29:14 +0200

    there is a compilation error

--- src/homework_6/App.java
+++ src/homework_6/App.java
@@ -4,16 +4,9 @@ import java.io.EOFException;
 import java.io.FileNotFoundException;
 
 public class App {
-    public static void main(String[] args) throws FileNotFoundException {
-        try {
-            if (System.currentTimeMillis() % 2 == 0) {
-                throw new EOFException();
-            } else {
-                throw new FileNotFoundException();
-            }
-        }
-        catch (EOFException e) {
-        }
+    public static void main(String[] args) throws Exception {
+       Throwable t = new Exception();
+       throw t;
     }
 
 }
