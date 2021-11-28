bf3b406a058c8ca515810fb229b8bb69647e683c
Your Name <you@example.com>
2021-11-28 14:26:54 +0200

    new version of code

--- src/homework_6/App.java
+++ src/homework_6/App.java
@@ -1,4 +1,20 @@
 package homework_6;
 
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+
 public class App {
+    public static void main(String[] args) throws FileNotFoundException {
+        try {
+            if (System.currentTimeMillis() % 2 == 0) {
+                throw new EOFException();
+            } else {
+                throw new FileNotFoundException();
+            }
+        }
+        catch (EOFException e) {
+        }
+    }
+
 }
+
